a13fa105538cc76523fb1492a3000c8456338d99
EricMurray-G00423903 <g00423903@atu.ie>
2025-05-18 21:10:34 +0100

    Add PublicUser struct and conversion implementation from User

--- src/user.rs
+++ src/user.rs
@@ -6,3 +6,19 @@ pub struct User {
     pub password_hash: String,
     pub created_at: chrono::DateTime<chrono::Utc>,
 }
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct PublicUser {
+    pub id: Uuid,
+    pub email: String,
+    pub created_at: chrono::DateTime<chrono::Utc>,
+}
+
+impl From<User> for PublicUser {
+    fn from(user: User) -> Self {
+        PublicUser { 
+            id: user.id, 
+            email: user.email, 
+            created_at: user.created_at 
+        }
+    }
+}
